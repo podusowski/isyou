d110a31486b23445b18c396435f34731ccaeaef4
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2021-01-07 20:20:04 +0100

    test that rocket is up

--- isyou_backend/sct/test_hello.py
+++ isyou_backend/sct/test_hello.py
@@ -1,18 +1,26 @@
 import pytest
 import subprocess
 from pathlib import Path
+import requests
 
 
 @pytest.fixture
 def isyou_backend():
     cargo_root = Path(__file__).parent.parent
     process = subprocess.Popen(["cargo", "run"], cwd=cargo_root)
-    yield
-    import time
-    time.sleep(0.5)
-    process.terminate()
-    process.wait()
+    try:
+        yield
+    finally:
+        process.terminate()
+        process.wait()
 
 
 def test_hello(isyou_backend):
-    pass
+    while True:
+        try:
+            r = requests.get("http://localhost:8000/")
+            break
+        except Exception as e:
+            print(e)
+            pass
+    print(r)
