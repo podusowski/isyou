08ee48ef48987d507026ab99532966ef6b0ebe30
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2021-01-09 19:58:25 +0100

    put seeks into a hash map

--- isyou_backend/src/main.rs
+++ isyou_backend/src/main.rs
@@ -12,34 +12,66 @@ fn health() -> &'static str {
 }
 
 #[derive(Clone, Serialize)]
+struct Point {
+    lat: f64,
+    lon: f64,
+}
+
+#[derive(Clone, Serialize)]
 struct Seek {
     id: usize,
+    points: Vec<Point>,
+}
+
+impl Seek {
+    fn new() -> Seek {
+        Seek {
+            id: 1,
+            points: Vec::new(),
+        }
+    }
 }
 
 #[derive(Clone, Serialize)]
 struct Seeks(Vec<Seek>);
 
 struct Global {
-    seeks: std::sync::Arc<std::sync::Mutex<Seeks>>,
+    seeks_by_id: std::sync::Arc<std::sync::Mutex<std::collections::HashMap<usize, Seek>>>,
 }
 
 impl Default for Global {
     fn default() -> Global {
         Global {
-            seeks: std::sync::Arc::new(std::sync::Mutex::new(Seeks(Vec::new()))),
+            seeks_by_id: std::sync::Arc::new(std::sync::Mutex::new(
+                std::collections::HashMap::new(),
+            )),
         }
     }
 }
 
+#[derive(Clone, Serialize)]
+struct SeekIds(Vec<usize>);
+
 #[get("/seeks")]
-fn seeks(global_state: rocket::State<Global>) -> Json<Seeks> {
-    Json(Seeks(global_state.seeks.lock().unwrap().0.clone()))
+fn seeks(global_state: rocket::State<Global>) -> Json<SeekIds> {
+    let ids = global_state
+        .seeks_by_id
+        .lock()
+        .unwrap()
+        .keys()
+        .map(|&x| x)
+        .collect();
+    Json(SeekIds(ids))
 }
 
 #[post("/seeks")]
 fn create_seek(global_state: rocket::State<Global>) {
-    global_state.seeks.lock().unwrap().0.push(Seek { id: 1 });
+    let mut seeks_by_id = global_state.seeks_by_id.lock().unwrap();
+    seeks_by_id.insert(1, Seek::new());
 }
+
+#[post("/seeks/<id>/points")]
+fn create_seek_point(global_state: rocket::State<Global>, id: usize) {}
 
 fn main() {
     rocket::ignite()
